b70f54bc46f0329cb7119d45cd9e2ced395fe17d
Simon McManus <mcmanus.simon@gmail.com>
2016-12-01 12:44:59 +0000

    add next edition to each page

--- spec.js
+++ spec.js
@@ -2,28 +2,61 @@ module.exports = {
   "/index.html": {
       page: 'home',
       spec: {
-          '#reviews': {
-              component: 'quotes'
+          '#next_edition': {
+              component: 'updates'
           }
       }
   },
   "/contact.html": {
     page: 'contact',
-          selectors: {
-          '.contact': {
-              className: 'active'
-          }
-      }
+    spec: {
+        '#next_edition': {
+             component: 'updates'
+              }
+    },
+    selectors: {
+    '.contact': {
+       className: 'active'
+    }
+    }
   },
    "/code-of-conduct.html": {
-    page: 'code-of-conduct'
+    page: 'code-of-conduct',
+        spec: {
+        '#next_edition': {
+             component: 'updates'
+              }
+    }
   },
-    "/terms.html": {
-    page: 'terms'
+  "/terms.html": {
+    page: 'terms',
+        spec: {
+        '#next_edition': {
+             component: 'updates'
+              }
+    }
   },
 
   options: {
       outputDir: '/docs',
-      files: ['/style/style.css']
+      files: [
+        'style/style.css',
+        'style/fonts.css',
+        'images/icons/chat_icon.svg',
+        'images/logo_main.svg',
+        'images/avatars/dwyl.jpeg',
+        'images/icons/pen_icon.svg',
+        'fonts/2E7DBB_3_0.ttf',
+        'fonts/2E7DBB_3_0.woff',
+        'fonts/2E7DBB_3_0.woff2',
+        'images/photo_bg.jpg',
+        'images/icons/email_icon.svg',
+        'images/icons/email_icon_w.svg',
+        'images/logo_symbol.svg',
+        'icons/email_icon_w.svg',
+        'images/dotjs-small.png',
+        'images/favicon.png'
+      ]
   }
 }
+
