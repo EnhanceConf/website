297427a63bafde416ebeaf0ebf81103db30a2f13
Simon McManus <mcmanus.simon@gmail.com>
2016-12-18 10:02:28 +0000

    adding offline

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 var router = require('speclate-router')
-
+var appCacheNanny = require('appcache-nanny')
 window.$ = require('jquery')
+
 router({
   after: function () {
     $('html,body').scrollTop($('#container'))
@@ -10,4 +11,20 @@ router({
       $container.html('<div class="markdown"><h1>Error</h1><p>Something went wrong fetching the page.</p><p>' + err + '</p></div>')
     }
   }
+})
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope)
+  }).catch(function (err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err)
+  })
+} else {
+  appCacheNanny.start()
+}
+
+appCacheNanny.on('updateready', function () {
+  location.reload()
 })
--- client/service-worker.js
+++ client/service-worker.js
@@ -1,5 +1,5 @@
 var serviceWorker = require('speclate-service-worker')
 var spec = require('../spec')
-var version = '1.1'
+var version = '1.3'
 
 serviceWorker(spec, version)
