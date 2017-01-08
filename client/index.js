4193be1fac8dfe5f5661e1b5633f0e353e732da0
Simon McManus <mcmanus.simon@gmail.com>
2017-01-08 11:35:33 +0000

    adding check for push state

--- client/index.js
+++ client/index.js
@@ -1,35 +1,43 @@
+
+
 var router = require('speclate-router')
 var appCacheNanny = require('appcache-nanny')
-window.$ = require('jquery')
 
-router({
-  after: function (error,  markup, page) {
-    var scrollTo = 0;
-    if(page.page !== 'home') {
-        scrollTo = $('#container').position().top - 30
+if(history.pushState) {
+
+    window.$ = require('jquery')
+
+    router({
+    after: function (error,  markup, page) {
+        var scrollTo = 0;
+        if(page.page !== 'home') {
+            scrollTo = $('#container').position().top - 30
+        }
+        $('html,body').scrollTop(scrollTo)
+
+    },
+    error: function (err, $container) {
+        if (err) {
+        $container.html('<div class="markdown"><h1>Error</h1><p>Something went wrong fetching the page.</p><p>' + err + '</p></div>')
+        }
     }
-    $('html,body').scrollTop(scrollTo)
+    })
 
-  },
-  error: function (err, $container) {
-    if (err) {
-      $container.html('<div class="markdown"><h1>Error</h1><p>Something went wrong fetching the page.</p><p>' + err + '</p></div>')
+    if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope)
+    }).catch(function (err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err)
+    })
+    } else {
+    appCacheNanny.start()
     }
-  }
-})
 
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
-//     // Registration was successful
-//     console.log('ServiceWorker registration successful with scope: ', registration.scope)
-//   }).catch(function (err) {
-//     // registration failed :(
-//     console.log('ServiceWorker registration failed: ', err)
-//   })
-// } else {
-//   appCacheNanny.start()
-// }
+    appCacheNanny.on('updateready', function () {
+    location.reload()
+    })
 
-// appCacheNanny.on('updateready', function () {
-//   location.reload()
-// })
+
+}
